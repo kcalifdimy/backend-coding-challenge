5dcea1bed1aca0e069db66c1e8a1012231cd5a57
Chibuzo Clifford Okorie <kcalifikz@gmail.com>
2023-01-10 04:31:56 +0100

    Update run_migrate_command.py

--- run_migrate_command.py
+++ run_migrate_command.py
@@ -4,20 +4,7 @@ from config import base
 
 #from conftest import Base, engine
 
-#planning.migrate_database_planning(base.FIXTURE_DIR_PATH)
+planning.migrate_database_planning(base.FIXTURE_DIR_PATH)
 
-#print("status: database migrated successfully")
+print("status: database migrated successfully")
 
-
-
-from commands import planning 
-from config.base import FILE_TEST_DIR_PATH
-
-engine = engine
-get_db = get_db() 
-db:Session = next(get_db)
-
-def migrate_database(FILE_TEST_DIR_PATH):
-    Base.metadata.create_all(bind=engine)
-    db.add_all(setupMigrationData(FILE_TEST_DIR_PATH))
-    db.commit()
